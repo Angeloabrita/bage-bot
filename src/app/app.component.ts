83649ab70b9635c2889693df8141bbf60407d6a3
agta <angeloabrita@gmail.com>
2025-01-28 10:41:14 -0300

    :spark: feet: creado navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { ChatComponent } from "./pages/chat/chat.component";
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ ChatComponent,
-    
+  imports: [ 
+    ChatComponent,
+    NavbarComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
